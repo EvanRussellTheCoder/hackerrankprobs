6dba66c60958130c9a5d1a2515edfaf6dccb1178
EvanRussellTheCoder <52358324+EvanRussellTheCoder@users.noreply.github.com>
2021-08-27 16:16:12 -0400

    Update compareTriplets.py

--- compareTriplets.py
+++ compareTriplets.py
@@ -11,17 +11,37 @@ def compareTriplets(a, b):
         bPoints = 0
         if(a[0] > b[0]):
             aPoints += 1
-        else:
+        elif(a[0] < b[0]):
             bPoints += 1
+        else:
+            pass
         if(a[1] > b[1]):
             aPoints += 1
-        else:
+        elif(a[1] < b[1]):
             bPoints += 1
+        else:
+            pass
         if(a[2] > b[2]):
             aPoints += 1
-        else:
+        elif(a[2] < b[2]):
             bPoints += 1
+        else:
+            pass
         return [aPoints, bPoints]
-    except:
+    except ValueError:
         print("I can't use this array, it needs to be three numbers")
+    except IndexError:
+        print("There's not enough of an input here, I need three numbers, not 0, 1 or 2")
+    
+#test cases
+#gets [0, 3]
+compareTriplets([1, 2, 3],[4, 5, 6])
+#gets [1, 0]
+compareTriplets([1, 2, 3],[1, 2, 1])
+#gets ValueError
+compareTriplets(["a,", "b", "c"],["d", "e", "f"])
+#gets IndexError
+compareTriplets([1, 2, 3][4, 5]) 
+        
+
     
